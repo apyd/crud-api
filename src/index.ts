ba23fb237017bb37eff808bbbb6d98bdf62a6592
apyd <arturpydych@gmail.com>
2024-02-17 17:45:57 +0100

    feat: add index.js with http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-const x = 4
+import http from 'http';
+import { getApiRoutes } from './routes';
+
+export const server = http.createServer();
+
+server.on("request", (req: http.IncomingMessage, res: http.ServerResponse) => {
+  const routes = getApiRoutes(req.url)
+  routes(req, res)
+})
+
+server.listen(process.env.PORT);
+console.log(`listening... on port ${process.env.PORT}`);
