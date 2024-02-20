5c6e341c2050e7e0dd574cbba9137904bf84e090
apyd <arturpydych@gmail.com>
2024-02-20 14:35:49 +0100

    chore: fix prod script and configure webpack

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import http from 'http';
+import http from 'node:http';
 import { getApiRoutes } from './routes';
 
-export const server = http.createServer();
+export const server = http.createServer()
 
 server.on("request", (req: http.IncomingMessage, res: http.ServerResponse) => {
   const routes = getApiRoutes(req.url)
   routes(req, res)
 })
 
-server.listen(process.env.PORT);
-console.log(`listening... on port ${process.env.PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`listening... on port ${process.env.PORT}`);
+});
