7409657729061b42d84d7294a75f121e34d3a476
apyd <arturpydych@gmail.com>
2024-02-20 17:07:33 +0100

    feat: implement utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,6 @@
+import http from 'node:http';
+import { ValidatedUserData } from '../models/users.model';
+
 export const isUUIDv4 = (id: unknown): boolean => {
   if(typeof id !== 'string' || (typeof id === 'string' && id?.length !== 36)) {
     return false;
@@ -19,4 +22,41 @@ export const validateUser = (userData: unknown): boolean => {
   }
 
   return true
+}
+
+export const getRequestBody = (req: http.IncomingMessage): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    let data = '';
+    req.on('data', (chunk) => data += chunk);
+    req.on('end', () => resolve(data));
+    req.on('error', (error) => reject(error));
+  });
+};
+
+export const getErrorMessage = (error: unknown) => {
+  if (error instanceof Error) return error.message
+  return String(error)
+}
+
+export const getValidatedUserData = (userData: unknown): ValidatedUserData => {
+  if(typeof userData !== 'object' || !userData) {
+    return null
+  }
+
+  const { username, age, hobbies } = userData as { username?: unknown, age?: unknown, hobbies?: unknown };
+  const filteredValidData: ValidatedUserData = {};
+
+  if (typeof username === 'string') {
+    filteredValidData.username = username;
+  }
+
+  if (typeof age === 'number') {
+    filteredValidData.age = age;
+  }
+
+  if (Array.isArray(hobbies)) {
+    filteredValidData.hobbies = hobbies;
+  }
+
+  return filteredValidData;
 }
